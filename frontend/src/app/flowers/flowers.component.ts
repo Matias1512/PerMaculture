3fcdc59ad8125c44fdcf52fd1f33912f3f821d6f
kroyoda <contact.mbm@protonmail.com>
2023-04-11 15:15:33 +0200

    update flowers component to use new plant service

--- frontend/src/app/flowers/flowers.component.ts
+++ frontend/src/app/flowers/flowers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Plant, PlantsService } from '../services/plants.service';
 
 @Component({
   selector: 'app-flowers',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./flowers.component.scss']
 })
 export class FlowersComponent {
+  plants: Plant[] = [];
+  
+  constructor(private plantService: PlantsService) {
+    this.showPlants();
+  }
 
+  showPlants() {
+    this.plantService.getPlants().subscribe((plants) => {
+      this.plants = plants;
+    });
+  }
 }
