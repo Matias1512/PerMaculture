6b4ca9d1f092cef96882a3eb9ea49a82fa27ddeb
kroyoda <contact.mbm@protonmail.com>
2023-04-25 11:13:50 +0200

    Register a Garden Keeper on connection page using ngForms

--- frontend/src/app/connexion/connexion.component.ts
+++ frontend/src/app/connexion/connexion.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Keeper } from '../models/keeper.interface'
+import { AuthentifierService } from '../services/authentifier.service'
 
 @Component({
-  selector: 'app-connexion',
-  templateUrl: './connexion.component.html',
-  styleUrls: ['./connexion.component.scss']
+	selector: 'app-connexion',
+	templateUrl: './connexion.component.html',
+	styleUrls: ['./connexion.component.scss']
 })
 export class ConnexionComponent {
+	submitted = false
+	model: Keeper = {
+		userName: '',
+		displayName: '',
+		email: '',
+		password: '',
+		isAdmin: false
+	}
 
+	constructor(private service: AuthentifierService) { }
+
+	onSubmit() {
+		this.submitted = true
+	}
+
+	newGardener() {
+		this.model = {
+			userName: '',
+			displayName: '',
+			email: '',
+			password: '',
+			isAdmin: false
+		}
+	}
 }
