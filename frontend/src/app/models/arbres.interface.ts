d54e0e4098631b1fa9721ac4a67f1badbdc78bd9
Brice MATIAS <bricematias3@gmail.com>
2023-04-25 08:45:12 +0200

    Création interface

---
+++ frontend/src/app/models/arbres.interface.ts
@@ -0,0 +1,6 @@
+export interface Arbres {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
---
+++ frontend/src/app/models/insectes.interface.ts
@@ -0,0 +1,6 @@
+export interface Insectes {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
