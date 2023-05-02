04d3a831b8d4790277eda5df95508686176237f7
Matias1512 <matias.caturla@gmail.com>
2023-05-02 11:56:23 +0200

    first version for modal flower

--- frontend/src/app/flowers/flower-modal/flower-modal.component.ts
+++ frontend/src/app/flowers/flower-modal/flower-modal.component.ts
@@ -1,11 +1,18 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Plant } from 'src/app/models/plant.interface';
+import { DialogData } from '../flowers.component';
 
 @Component({
   selector: 'app-flower-modal',
   templateUrl: './flower-modal.component.html',
   styleUrls: ['./flower-modal.component.scss']
 })
-export class FlowerModalComponent {
-  constructor( private dialog: MatDialogRef<FlowerModalComponent> ) {}
-}
+export class FlowerModalComponent implements OnInit {
+  constructor( 
+    private dialog: MatDialogRef<FlowerModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, ) {}
+
+  ngOnInit(): void {
+  }
+} 
