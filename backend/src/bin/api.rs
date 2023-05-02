6820ec32c9241429460c7fa0d1b2d3bcede6768f
kroyoda <contact.mbm@protonmail.com>
2023-05-02 10:16:14 +0200

    Update binaries and library

--- backend/src/bin/api.rs
+++ backend/src/bin/api.rs
@@ -1,11 +1,11 @@
 use std::{env, net::SocketAddr};
 
-use axum::{Router, routing::{get, patch}, Extension};
+use axum::{Router, routing::{get, patch, post}, Extension};
 use deadpool_diesel::{postgres::{Manager, Pool}, Runtime};
 use tower_http::cors::CorsLayer;
 use dotenvy::dotenv;
 
-use backend::handlers::plants::{get_plants, create_plant, remove_plant, update_plant};
+use backend::handlers::{plants::{get_plants, create_plant, remove_plant, update_plant, get_plant}, keepers::{get_keepers, create_keeper, update_keeper, remove_keeper, get_keeper, login}};
 
 fn main() {
     dotenv().ok();
@@ -27,7 +27,23 @@ fn main() {
                 )
                 .route("/plants/:id",
                     patch(update_plant)
-                    .delete(remove_plant))
+                    .delete(remove_plant)
+                    .get(get_plant)
+                )
+                .route(
+                    "/login",
+                    post(login)
+                )
+                .route("/keepers",
+                    get(get_keepers)
+                    .post(create_keeper)
+                )
+                .route(
+                    "/keepers/:id",
+                    patch(update_keeper)
+                    .delete(remove_keeper)
+                    .get(get_keeper)
+                )
                 .layer(CorsLayer::permissive())
                 .layer(Extension(pool));
             let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
