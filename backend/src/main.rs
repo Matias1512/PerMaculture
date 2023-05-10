a726052bc70ff2398de12a5c19c1a64e4b340795
kroyoda <contact.mbm@protonmail.com>
2023-05-11 01:24:00 +0200

    Run diesel migration on startup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -11,6 +11,9 @@ use handlers::plants::*;
 use handlers::trees::*;
 
 use models::Db;
+use rocket::Build;
+use rocket::Rocket;
+use rocket::fairing::AdHoc;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -22,10 +25,24 @@ fn hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+async fn run_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
+    use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
+    const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
+    Db::get_one(&rocket).await
+        .expect("Expected database connection")
+        .run(|conn| {
+            conn.run_pending_migrations(MIGRATIONS)
+                .expect("Failed to run database migrations");
+        }).await;
+    rocket
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Db::fairing())
+        .attach(AdHoc::on_ignite("Diesel Postgresql Migrations", run_migrations))
         .attach(Cors)
         .mount("/", routes![index, hello, all_options])
         .mount(
