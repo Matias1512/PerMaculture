9a7cff5da6fa0567949d58a72f9d49b1505f2869
kroyoda <contact.mbm@protonmail.com>
2023-05-11 22:15:55 +0200

    Created field parcels

--- frontend/src/app/potager/potager.component.ts
+++ frontend/src/app/potager/potager.component.ts
@@ -1,5 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+type Item = {
+  name: string;
+  quantity: number;
+};
+
+type WarehouseParcel = {
+  id: number;
+  name: string;
+  empty?: boolean;
+};
 
 @Component({
   selector: 'app-potager',
@@ -7,6 +17,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./potager.component.scss']
 })
 export class PotagerComponent {
-  
+  parcels: WarehouseParcel[] = [];
+  selectedParcel: WarehouseParcel | null = null;
+
+  items: Item[] = [
+    { name: 'Tomate', quantity: 10 },
+    { name: 'Salade', quantity: 5 },
+    { name: 'Carotte', quantity: 3 },
+    { name: 'Radis', quantity: 2 },
+    { name: 'Pomme de terre', quantity: 1 },
+    { name: 'Poivron', quantity: 1 },
+    { name: 'Aubergine', quantity: 1 },
+    { name: 'Courgette', quantity: 1 },
+    { name: 'Oignon', quantity: 1 },
+  ];
+
+  constructor() {
+    for (let i = 0; i < 24; i++) {
+      this.parcels.push({
+        id: i,
+        name: `Parcelle ${i}`
+      });
+    }
+  }
+
+  onClickParcel(parcel: WarehouseParcel) {
+    this.selectedParcel = parcel;
+    console.log(parcel);
+  }
 }
- 
